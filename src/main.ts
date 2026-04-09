b87337a2d5cc7312b9c3cf7d4cfea676182bbf15
sab0c <sabrina.oliveira.clemente@gmail.com>
2026-04-09 15:41:20 -0300

    chore: initialize NestJS backend with basic environment setup

---
+++ src/main.ts
@@ -0,0 +1,12 @@
+import { NestFactory } from '@nestjs/core';
+import { ConfigService } from '@nestjs/config';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService);
+  const port = Number(configService.get<string>('PORT')) || 3000;
+
+  await app.listen(port);
+}
+void bootstrap();
